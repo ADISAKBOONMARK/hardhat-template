c4374e8316012fb45a1d076990694a3135b982bc
ADISAKBOONMARK <adisak0661013@gmail.com>
2022-03-15 22:32:57 +0700

    Remove comment code

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,7 +1,2 @@
-// before(async () => {
-//   const config = require("../config/Config.test.js");
-//   global._config = await config.setting();
-// });
-
 require("./MyToken20.test.js");
 require("./Airdrop.test.js");
